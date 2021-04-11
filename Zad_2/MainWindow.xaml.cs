ad6d61c616c38e8d1fe45c75c46f535463301c54
MihailHristov476 <mihailhristov2347@gmail.com>
2021-04-11 13:52:50 +0300

    Добавьте файлы проекта.

---
+++ Zad_2/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zad_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string s = textBoxMass.Text;
+            string[] arr = s.Split(' ');
+            int[] myArr = Array.ConvertAll(arr, n => int.Parse(n));
+
+            int summa = 0;
+
+            for(int i = 0; i < myArr.Length; i++)
+            {
+                if (i%2 !=0)
+                {
+                    summa += myArr[i];
+                }
+            }
+
+
+            textBoxTotal.Text = summa.ToString();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string s = textBoxMass.Text;
+            string[] s1 = s.Split(' ');
+            int[] myArr = Array.ConvertAll(s1, n => int.Parse(n));
+
+            string a = textBoxA.Text;
+            int b = Int32.Parse(a);
+
+            int t = 0;
+            for(int i = 0; i<myArr.Length; i++)
+            {
+                if(myArr[i]<b && myArr[i] % 5 == 0)
+                {
+                    t++;
+                }
+            }
+
+            textBoxTotal.Text = t.ToString();
+
+        }
+
+        
+    }
+}
